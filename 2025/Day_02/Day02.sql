8c65ca7ccb7706fa46b385be942699eeeefb88a1
acohen-fortifieddata <acohen@fortifieddata.com>
2025-12-02 08:36:57 -0800

    2025 Day 2

--- 2025/Day_02/Day02.sql
+++ 2025/Day_02/Day02.sql
@@ -1,4 +1,4 @@
-declare @Input varchar(max) = '19391-47353,9354357-9434558,4646427538-4646497433,273-830,612658-674925,6639011-6699773,4426384-4463095,527495356-527575097,22323258-22422396,412175-431622,492524-611114,77-122,992964846-993029776,165081-338962,925961-994113,7967153617-7967231799,71518058-71542434,64164836-64292066,4495586-4655083,2-17,432139-454960,4645-14066,6073872-6232058,9999984021-10000017929,704216-909374,48425929-48543963,52767-94156,26-76,1252-3919,123-228'
+declare @Input varchar(max) = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'
 
 drop table if exists #Input
 select ordinal, cast(parsename(n1, 2) as bigint) Num1, cast(parsename(n1, 1) as bigint) Num2
@@ -19,16 +19,14 @@ where l1%2 = 0
 	(select *, max(len(cast(Num2 as varchar(max)))) over() MaxLen
 		from #Input
 	)
-	, i1 as
-	(select distinct ordinal, v1.[value]
-		from i
-			cross apply generate_series(Num1, Num2) v1
-			cross apply (select cast(v1.[value] as varchar(100)) n1) n
-			cross apply (select len(n1) l1) l
-			cross apply generate_series(cast(1 as int), cast(MaxLen/2 as int)) v2
-		where l1 > v2.[value]
-			and l1%v2.[value] = 0
-			and n1 = replicate(left(n1, v2.[value]), l1/v2.[value])
-	)
-select sum([value])
-from i1
+select sum(v1.[value])
+from i
+	cross apply generate_series(Num1, Num2) v1
+	cross apply (select cast(v1.[value] as varchar(100)) n1) n
+	cross apply (select len(n1) l1) l
+where exists (select *
+				from generate_series(cast(1 as int), cast(MaxLen/2 as int)) v2
+				where l1 > v2.[value]
+					and l1%v2.[value] = 0
+					and n1 = replicate(left(n1, v2.[value]), l1/v2.[value])
+			)
