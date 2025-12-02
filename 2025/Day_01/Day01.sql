c6574e957d70a6d97fa881fe31210cdd3c9c601e
acohen-fortifieddata <acohen@fortifieddata.com>
2025-12-01 19:21:53 -0800

    2025 Day 1

--- 2025/Day_01/Day01.sql
+++ 2025/Day_01/Day01.sql
@@ -15,19 +15,19 @@ select cast(ordinal as int) ordinal, left([value], 1) dir, cast(substring([value
 into #Input
 from string_split(replace(@Input, char(10), ''), char(13), 1)
 
-;with rec as
-	(select cast(0 as int) step, cast(50 as int) loc
+;with i as
+	(select *
+		from #Input
 		union all
-		select cast(i.ordinal as int), FinalPosition
-		from rec r
-			inner join #Input i on i.ordinal = r.step + 1
-			cross apply (select cast(loc + iif(i.dir = 'L', -1, 1)*(i.steps%100) as int) BaseCalc) b
-			cross apply (select (BaseCalc+100)%100 FinalPosition) f
+		select 0, '', 50
+	)
+	, i1 as
+	(select *, (sum(iif(dir = 'L', -1, 1)*(steps%100)) over(order by ordinal rows between unbounded preceding and current row) + 100)%100 FinalPosition
+		from i
 	)
 select count(*) Solution1
-from rec
-where loc = 0
-option (maxrecursion 32767)
+from i1
+where FinalPosition = 0
 
 ;with rec as
 	(select cast(0 as int) step, cast(50 as int) loc, 0 Points
